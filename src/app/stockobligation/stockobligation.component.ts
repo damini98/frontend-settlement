38bb3ed4f0995ab5dc3a97da217b2492e3724241
Saurav MAhale <mlaxman@mt.iitr.ac.in>
2020-09-29 16:58:29 +0530

    service stockobligation

--- src/app/stockobligation/stockobligation.component.ts
+++ src/app/stockobligation/stockobligation.component.ts
@@ -1,34 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { StockserviceService } from './stockservice.service';
+import {Observable} from 'rxjs';
 export interface StockObligation {
   name: string;
-  google: number; //TODO:apple
+  google: number;
   amazon: number;
   apple: number;
   netflix: number;
   facebook: number;
 }
-
-const ELEMENT_DATA: StockObligation[] = [
-  {name: 'Citi', google: 34, amazon: 66,apple:89,netflix:123,facebook:78},
-  {name: 'DB', google: 34, amazon: 66,apple:89,netflix:123,facebook:78},
-  {name: 'Citi', google: 34, amazon: 66,apple:89,netflix:123,facebook:78},
-  {name: 'Citi', google: 34, amazon: 66,apple:89,netflix:123,facebook:78},
-  {name: 'Citi', google: 34, amazon: 66,apple:89,netflix:123,facebook:78},
-  
-
-
-];
 @Component({
   selector: 'app-stockobligation',
   templateUrl: './stockobligation.component.html',
   styleUrls: ['./stockobligation.component.css']
 })
-export class StockobligationComponent implements OnInit {displayedColumns: string[] = [ 'name', 'google', 'amazon','apple','netflix','facebook'];
-dataSource = ELEMENT_DATA;
+export class StockobligationComponent implements OnInit {displayedColumns: string[] = ['name', 'google','amazon','apple','netflix','facebook'];
+// dataSource = ELEMENT_DATA;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+public getData=[] //obs
+constructor(private _httpService: StockserviceService) { }
+ngOnInit(): void {
+  this._httpService.getStock().subscribe(data => this.getData = data);
 }
+}
+
