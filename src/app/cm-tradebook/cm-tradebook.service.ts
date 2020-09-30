b2ab2bbcba5588f0cf0bbfff2fd25a40ef50f987
damini98 <daminisheth98@gmail.com>
2020-09-30 17:24:56 +0530

    ok

--- src/app/cm-tradebook/cm-tradebook.service.ts
+++ src/app/cm-tradebook/cm-tradebook.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import {Observable} from 'rxjs';
 import { CmTradebookComponent } from './cm-tradebook.component';
 // import { GlobalData } from '../assets/data/global-data';
@@ -8,11 +8,9 @@ import { CmTradebookComponent } from './cm-tradebook.component';
   providedIn: 'root'
 })
 export class CmTradebookService {
-
   constructor(private http:HttpClient){} 
   getCmTradebook():Observable<CmTradebookComponent[]> {
-    
-    // console.log();
-    return this.http.get<CmTradebookComponent[]>("http://localhost:8888/cm-tradebook?cmid=4")
+    const params = new HttpParams().set('cmid', '2');
+    return this.http.get<CmTradebookComponent[]>( "http://localhost:8888/cm-tradebook", {params});
   }
 }
